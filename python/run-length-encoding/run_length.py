f70042cb99d499b4431fd2df7488755d81649827
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-14 17:48:35 -0200

    fix bug compress 1-char "sequence"

--- python/run-length-encoding/run_length.py
+++ python/run-length-encoding/run_length.py
@@ -7,19 +7,24 @@ def encode(source):
         while  pivot < len(source) and char == source[pivot]:
             count += 1
             pivot += 1
-        result += str(count) + char
+        if count > 1:
+            result += str(count) + char
+        else:
+            result += char
         count = 0
     return result
 
 def decode(source):
     result = ""
     number = "0"
-    looking_for = "DIGIT"
     for char in source:
         if char.isdigit():
             number += char
         else:
-            number = int(number)
+            if len(number) > 0:
+                number = int(number)
+            else:
+                number = 1
             result += (char * number)
             number = ""
     return result
