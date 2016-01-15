25d526575089ddcfb300a3fba57ccacc12f69808
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-15 15:00:25 -0200

    another hamming alternative

--- haskell/point-mutations/DNA.hs
+++ haskell/point-mutations/DNA.hs
@@ -1,4 +1,4 @@
 module DNA(hammingDistance) where
 
 hammingDistance :: (Eq a) => [a] -> [a] -> Int
-hammingDistance a = length . filter id . zipWith (/=) a
+hammingDistance a = length . filter not . zipWith (==) a
