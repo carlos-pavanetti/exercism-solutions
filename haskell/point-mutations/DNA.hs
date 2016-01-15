54e9351f5c33f7bb9932b44a9330ed50ef466016
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-15 15:05:19 -0200

    so point free

--- haskell/point-mutations/DNA.hs
+++ haskell/point-mutations/DNA.hs
@@ -1,4 +1,4 @@
 module DNA(hammingDistance) where
 
 hammingDistance :: (Eq a) => [a] -> [a] -> Int
-hammingDistance a = length . filter not . zipWith (==) a
+hammingDistance = (length .) . (filter not .) . zipWith (==)
