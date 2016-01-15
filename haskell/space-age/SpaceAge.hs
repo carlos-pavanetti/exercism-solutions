96939289b39cc0c9a0a53335afc1da4d632b01bf
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-15 15:46:35 -0200

    improved spaceage haskell problem

--- haskell/space-age/SpaceAge.hs
+++ haskell/space-age/SpaceAge.hs
@@ -3,11 +3,11 @@ module SpaceAge(Planet(..), ageOn) where
 data Planet = Mercury | Venus | Earth | Mars | Jupiter | Saturn | Uranus | Neptune
 
 ageOn :: (Fractional c, Num c) => Planet -> c -> c
-ageOn Mercury seconds = seconds / 7600543.81992
-ageOn Venus   seconds = seconds / 19414149.052176
+ageOn Mercury seconds = seconds / (31557600 * 0.2408467)
+ageOn Venus   seconds = seconds / (31557600 * 0.61519726)
 ageOn Earth   seconds = seconds / 31557600
-ageOn Mars    seconds = seconds / 59354032.69008
-ageOn Jupiter seconds = seconds / 374355659.124
-ageOn Saturn  seconds = seconds / 929292362.8848
-ageOn Uranus  seconds = seconds / 2651370019.3296
-ageOn Neptune seconds = seconds / 5200418560.0320005
+ageOn Mars    seconds = seconds / (31557600 * 1.8808158)
+ageOn Jupiter seconds = seconds / (31557600 * 11.862615)
+ageOn Saturn  seconds = seconds / (31557600 * 29.447498)
+ageOn Uranus  seconds = seconds / (31557600 * 84.016846)
+ageOn Neptune seconds = seconds / (31557600 * 164.79132)
