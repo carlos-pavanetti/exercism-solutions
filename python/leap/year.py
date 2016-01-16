c944faa3d16287cb940cbfd5900c5a2694cb31aa
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-16 18:41:16 -0200

    refactor python leap year problem

--- python/leap/year.py
+++ python/leap/year.py
@@ -1,8 +1,4 @@
 def is_leap_year(year):
-    if not year % 4 == 0:
-        return False
-    if not year % 100 == 0:
-        return True
-    if not year % 400 == 0:
-        return False
-    return True
+    if year % 100 == 0:
+        return year % 400 == 0
+    return year % 4 == 0
