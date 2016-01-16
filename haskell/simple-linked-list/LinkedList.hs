93a8b9ca482e5c9ff298a01092f7a48f1a5d28b9
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-16 01:24:48 -0200

    refactor of haskell linked list

--- haskell/simple-linked-list/LinkedList.hs
+++ haskell/simple-linked-list/LinkedList.hs
@@ -1,12 +1,11 @@
 module LinkedList (
     LinkedList(..),
     nil, isNil, new,
-    next, datum,
     toList, fromList,
     reverseLinkedList
 ) where
 
-data LinkedList a = Nil | Node a (LinkedList a) deriving(Show)
+data LinkedList a = Nil | Node { datum :: a, next :: LinkedList a } deriving (Show)
 
 nil :: LinkedList a
 nil = Nil
@@ -18,19 +17,12 @@ isNil _ = False
 new :: a -> LinkedList a -> LinkedList a
 new = Node
 
-next :: LinkedList a -> LinkedList a
-next (Node _ nxt) = nxt
-
-datum :: LinkedList a -> a
-datum (Node value _) = value
-
 toList :: LinkedList a -> [a]
 toList Nil = []
 toList (Node value nxt) = value : toList nxt
 
 fromList :: [a] -> LinkedList a
-fromList [] = Nil
-fromList (x:xs) = Node x (fromList xs)
+fromList = foldr Node Nil
 
 reverseLinkedList :: LinkedList a -> LinkedList a
 reverseLinkedList = fromList . reverse . toList
