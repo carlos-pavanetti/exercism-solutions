74f3edbea721243533f02b10b13d008289168a27
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-16 18:39:27 -0200

    refactor haskell word count

--- haskell/word-count/WordCount.hs
+++ haskell/word-count/WordCount.hs
@@ -5,5 +5,12 @@ import Data.List(sort, group)
 import Data.Map(Map, fromList)
 
 wordCount :: String -> Map String Int
-wordCount = fromList . map (\xs@(x:_) -> (x, length xs)) . group . sort . words . filter (not . blacklist) . map toLower
-    where blacklist s = isSymbol s || isPunctuation s
+wordCount = fromList . map (\xs@(x:_) -> (x, length xs)) . group . sort . toWords . map toLower
+
+toWords :: String -> [String]
+toWords s = case dropWhile blacklist s of
+    "" -> []
+    s' -> w : toWords s''
+        where (w, s'') = break blacklist s'
+    where
+        blacklist c = isSymbol c || isPunctuation c || isSpace c
