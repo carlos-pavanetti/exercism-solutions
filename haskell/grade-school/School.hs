b2759a0582c62a1d87d940f0e634fed507571544
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-16 00:14:59 -0200

    refactor of grade-school haskell problem

--- haskell/grade-school/School.hs
+++ haskell/grade-school/School.hs
@@ -2,13 +2,16 @@ module School(School, empty, add, sorted, grade) where
 
 import Data.List(sort)
 
-data School = Empty | Grade Int [String] School
-type Grade = (Int, [String])
+type GradeNumber = Int
+type Student = String
+data School = Empty | Grade GradeNumber [Student] School
+
+type Grade = (GradeNumber, [Student])
 
 empty :: School
 empty = Empty
 
-add :: Int -> String -> School -> School
+add :: GradeNumber -> Student -> School -> School
 add gradeNum student Empty = Grade gradeNum [student] empty
 add gradeNum student (Grade thisGrade list school)
     | gradeNum == thisGrade = Grade thisGrade (student:list) school
@@ -23,7 +26,7 @@ sorted (Grade gradeNum list school) =
         minorGrades  = [grd | grd <- sortedSchool, fst grd < gradeNum]
         majorGrades  = [grd | grd <- sortedSchool, fst grd >= gradeNum]
 
-grade :: Int -> School -> [String]
+grade :: GradeNumber -> School -> [Student]
 grade _ Empty = []
 grade gradeNum (Grade thisGrade list school)
     | gradeNum == thisGrade = list
