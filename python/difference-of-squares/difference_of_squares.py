4843336cdc2dffa0365fbb2e1daab59835426748
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-18 00:40:26 -0200

    python difference of squares alternative
    With sum of ranges, without math hack

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,9 +1,10 @@
+# without math hack
 def sum_of_squares(n):
-    return (n * (n + 1) * (2 * n + 1)) / 6
+    return sum(x*x for x in range(n + 1))
 
 
 def square_of_sum(n):
-    return (n * (n + 1) / 2) ** 2
+    return sum(range(n + 1)) ** 2
 
 
 def difference(n):
