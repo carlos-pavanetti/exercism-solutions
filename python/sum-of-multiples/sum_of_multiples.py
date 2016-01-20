288bc74286733ffa6120dcb28217a577e3a36f2a
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-20 21:56:24 -0200

    correted python sum of multiples

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,4 +1,5 @@
 def sum_of_multiples(limit, factors=[3, 5]):
-    factors = filter(factor != 0 for factor in factors)
+    if factors[0] == 0:
+        factors = factors[1:]
     return sum(number for number in range(limit)
                if any(number % factor == 0 for factor in factors))
