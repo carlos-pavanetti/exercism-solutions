6e179e92300984cf99c92f872260805e6e0af923
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-20 15:40:08 -0200

    binary to decimal with bit shift

--- haskell/binary/Binary.hs
+++ haskell/binary/Binary.hs
@@ -1,10 +1,12 @@
 module Binary(toDecimal) where
 
+import Data.Bits(shiftL)
+
 toDecimal :: String -> Int
 toDecimal bin
     | not.all (`elem` "01") $ bin = 0
     | otherwise = foldr shift 0 (reverse bin)
     where
         shift x acc = if x == '1'
-            then 2*acc + 1
-            else 2*acc
+            then acc `shiftL` 1 + 1
+            else acc `shiftL` 1
