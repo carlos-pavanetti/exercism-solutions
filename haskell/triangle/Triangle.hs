7ed969991d6ee5f41e5081abac1c129b068829f7
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-19 23:14:27 -0200

    triangle problem with Data.List(nub)
    How many odds sides are calculated with nub

--- haskell/triangle/Triangle.hs
+++ haskell/triangle/Triangle.hs
@@ -1,13 +1,16 @@
 module Triangle(TriangleType(..), triangleType) where
 
+import Data.List(nub)
+
 data TriangleType = Equilateral | Isosceles | Scalene | Illogical deriving (Eq, Show)
 
 triangleType :: Int -> Int -> Int -> TriangleType
 triangleType a b c
     | not (isTriangle a b c) = Illogical
-    | all (== a) [b, c] = Equilateral
-    | b == c || c == a || a == b = Isosceles
+    | uniques == 0 = Equilateral
+    | uniques == 1 = Isosceles
     | otherwise = Scalene
+    where uniques = length (nub [a, b, c]) - 1
 
 isTriangle :: Int -> Int -> Int -> Bool
 isTriangle a b c = sum [a, b, c] > 2 * maximum [a, b, c]
