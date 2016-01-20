39d5867867671d99459931ae64dfcda0490c2bb0
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-20 21:46:25 -0200

    python sum of multiples alternatives

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,7 +1,4 @@
 def sum_of_multiples(limit, factors=[3, 5]):
-    total = 0
-    factors = [factor for factor in factors if factor != 0]
-    for number in range(limit):
-        if any(map(lambda fac: number % fac == 0, factors)):
-            total += number
-    return total
+    factors = filter(factor != 0 for factor in factors)
+    return sum(number for number in range(limit)
+               if any(number % factor == 0 for factor in factors))
