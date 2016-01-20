001ac53d37a195708b28ed8b2de25dd4d58a85cc
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-19 23:22:33 -0200

    correction of uniques helper

--- haskell/triangle/Triangle.hs
+++ haskell/triangle/Triangle.hs
@@ -10,7 +10,11 @@ triangleType a b c
     | uniques == 0 = Equilateral
     | uniques == 1 = Isosceles
     | otherwise = Scalene
-    where uniques = length (nub [a, b, c]) - 1
+    where
+        uniques = if length_nub < length [a, b, c]
+            then length_nub - 1
+            else length_nub
+        length_nub = length (nub [a, b, c])
 
 isTriangle :: Int -> Int -> Int -> Bool
 isTriangle a b c = sum [a, b, c] > 2 * maximum [a, b, c]
