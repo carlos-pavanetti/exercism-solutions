19e421a2648ccdf20dfe85bedc9ab363d288378e
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-20 19:34:31 -0200

    refactor raindrops

--- haskell/raindrops/Raindrops.hs
+++ haskell/raindrops/Raindrops.hs
@@ -1,10 +1,10 @@
 module Raindrops(convert) where
 
 convert :: Int -> String
-convert number = sandbox $ pling ++ plang ++ plong
+convert number = if null answer then show number else answer
     where
         pling = if number `isDivisibleBy` 3 then "Pling" else ""
         plang = if number `isDivisibleBy` 5 then "Plang" else ""
         plong = if number `isDivisibleBy` 7 then "Plong" else ""
-        sandbox x = if x /= "" then x else show number
+        answer = pling ++ plang ++ plong
         n `isDivisibleBy` q = n `mod` q == 0
