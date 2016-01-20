c21f6da49cd3c8d6986453fde9ad84f5b6ff64d2
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-20 00:45:53 -0200

    python iterative n-digit series

--- python/series/series.py
+++ python/series/series.py
@@ -3,11 +3,10 @@ def slices(word, size):
         raise ValueError("Word is shorter than serie required")
     if size == 0:
         raise ValueError("0-digit series is invalid")
-    return slices_helper(word, size)
+    series = []
+    while len(word) >= size:
+        serie = [int(digit) for digit in word[:size]]
+        series.append(serie)
 
-
-def slices_helper(word, size):
-    if len(word) < size:
-        return []
-    serie = [int(digit) for digit in word[:size]]
-    return [serie] + slices_helper(word[1:], size)
+        word = word[1:]
+    return series
