4be8435687fa0b47d89d4538f11f84344a4a4255
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-20 15:46:37 -0200

    binary to decimal with left fold

--- haskell/binary/Binary.hs
+++ haskell/binary/Binary.hs
@@ -1,12 +1,10 @@
 module Binary(toDecimal) where
 
 import Data.Bits(shiftL)
+import Data.Char(digitToInt)
 
 toDecimal :: String -> Int
 toDecimal bin
     | not.all (`elem` "01") $ bin = 0
-    | otherwise = foldr shift 0 (reverse bin)
-    where
-        shift x acc = if x == '1'
-            then acc `shiftL` 1 + 1
-            else acc `shiftL` 1
+    | otherwise = foldl shift 0 bin
+    where shift acc x = acc `shiftL` 1 + digitToInt x
