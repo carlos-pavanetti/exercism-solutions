a0d12474896888678c2170552681a24ff77b6551
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-21 00:35:19 -0200

    atbash cipher with Data.List.Split(chunksOf)

--- haskell/atbash-cipher/Atbash.hs
+++ haskell/atbash-cipher/Atbash.hs
@@ -1,18 +1,13 @@
 module Atbash(encode) where
 
 import Data.Char
+import Data.List.Split(chunksOf)
 
 encode :: String -> String
-encode = space5 . map (cipher . toLower) . filter isAlphaNum
+encode = unwords . chunksOf 5 . map (cipher . toLower) . filter isAsciiAlphaNum
     where
         cipher x
             | isAlpha x = chr (against - ord x)
             | otherwise = x
         against = ord 'a' + ord 'z'
-
-space5 :: String -> String
-space5 str
-    | length str <= 5 = str
-    | otherwise = now ++ " " ++ space5 next
-    where
-        (now, next) = splitAt 5 str
+        isAsciiAlphaNum x = isAscii x && isAlphaNum x
