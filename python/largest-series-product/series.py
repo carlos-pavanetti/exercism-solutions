7742cc4b1d06c63296b09dd2737fdd695ac912a7
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-23 01:06:22 -0200

    function style in largest series product

--- python/largest-series-product/series.py
+++ python/largest-series-product/series.py
@@ -1,3 +1,7 @@
+from functools import reduce
+import operator
+
+
 def str_to_digit(string):
     return [int(digit) for digit in string]
 
@@ -5,18 +9,15 @@ def str_to_digit(string):
 def slices(string, size):
     if len(string) < size:
         raise ValueError("Slice size could not be bigger than string")
+
     str_slices = [string[i:i+size] for i in range(len(string) - size + 1)]
     num_slices = [str_to_digit(i) for i in str_slices]
     return num_slices
 
 
 def largest_product(string, size):
-    num_slices = slices(string, size)
-    largest_prod = 1
-    for slice_group in num_slices:
-        product = 1
-        for number in slice_group:
-            product *= number
-        if product > largest_prod:
-            largest_prod = product
-    return largest_prod
+    if size is 0:
+        return 1
+
+    slices_of_numbers = slices(string, size)
+    return max(reduce(operator.mul, group) for group in slices_of_numbers)
