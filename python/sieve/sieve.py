c36e8f17b845f8d44dde73f3c70a9236b1603b9e
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-22 23:08:07 -0200

    refactor sieve in python

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -2,36 +2,21 @@ from math import ceil, sqrt
 
 
 def sieve(limit):
-    Undefined = 0
-    NotPrime = 1
-    Prime = 2
-    candidates = {number: Undefined for number in range(2, limit + 1)}
+    candidates = {number: True for number in range(2, limit + 1)}
 
     def isUndefined(x):
-        return candidates[x] == Undefined
+        return candidates[x] is True
 
     def isPrime(x):
-        return candidates[x] == Prime
+        return candidates[x] is True
 
-    candidates[2] = Prime
-    current_prime = 2
+    def setAsNotPrime(x):
+        candidates[x] = False
+
     sqrt_limit = ceil(sqrt(limit))
+    for i in range(2, sqrt_limit):
+        if isUndefined(i):
+            for j in range(i**2, limit+1, i):
+                setAsNotPrime(j)
 
-    while current_prime < sqrt_limit:
-        # marking
-        for number in candidates:
-            if isUndefined(number) and number % current_prime == 0:
-                candidates[number] = NotPrime
-
-        # find next unmarked
-        for number in range(current_prime, limit + 1):
-            if isUndefined(number):
-                candidates[number] = Prime
-                current_prime = number
-                break
-
-    # mark unmarked as prime
-    for number in range(current_prime, limit + 1):
-        if isUndefined(number):
-            candidates[number] = Prime
-    return [number for number in candidates if isPrime(number)]
+    return [x for x in range(2, limit + 1) if isPrime(x)]
