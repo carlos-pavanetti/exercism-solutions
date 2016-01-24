361c3aa8c421e83a62da8ab14eeab4cf8a007ef6
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-24 01:12:54 -0200

    refactor grade-school with defaultdict collection

--- python/grade-school/school.py
+++ python/grade-school/school.py
@@ -1,28 +1,22 @@
+from collections import defaultdict
+
+
 class School(object):
     def __init__(self, name=""):
-        self.db = {}
-        self.name = name
+        self.db = defaultdict(set)
+        self.name = ""
 
     def add(self, student, grade):
-        if grade not in self.db:
-            self.db[grade] = {student}
-        else:
-            self.db[grade].update({student})
+        self.db[grade].update({student})
 
     def grade(self, grade):
-        try:
-            return self.db[grade]
-        except:
-            return set()
+        return self.db[grade]
 
     def sort(self):
         school = []
         for grade, students in self.db.items():
-            try:
-                students = list(students)
-                students.sort()
-                school.append((grade, tuple(students)))
-            except:
-                print(students)
+            students = list(students)
+            students.sort()
+            school.append((grade, tuple(students)))
 
         return school
