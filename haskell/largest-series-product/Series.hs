e5a71f0e2974465f6ae90440dcb0d7994b0cfb6c
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-25 19:07:43 -0200

    corrected haskell largest product series

--- haskell/largest-series-product/Series.hs
+++ haskell/largest-series-product/Series.hs
@@ -1,20 +1,17 @@
-{-# LANGUAGE UnicodeSyntax #-}
-
 module Series (digits, slices, largestProduct) where
 
 import Data.Char(digitToInt)
 
-digits ∷ String → [Int]
+digits :: String -> [Int]
 digits = map digitToInt
 
-slices ∷ Int → String → [[Int]]
+slices :: Int -> String -> [[Int]]
 slices _ [] = []
-slices n xss@(_:xs)
-    | n > length xss = []
-    | otherwise = digits (take n xss) : slices n xs
+slices n xss@(_:xs) =
+    takeWhile ((== n) . length) (digits (take n xss) : slices n xs)
 
-largestProduct ∷ Int → String → Int
+largestProduct :: Int -> String -> Int
 largestProduct n xs
-    | xs == "" = 1
-    | n > length xs = 1
-    | otherwise = maximum . map product . slices n $ xs
+    | null slices_xs = 1
+    | otherwise = maximum (map product slices_xs)
+    where slices_xs = slices n xs
