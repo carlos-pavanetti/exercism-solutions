229131ac87f0e65c58c41946ed9f189ae59e652c
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-25 22:02:44 -0200

    strict version of etl in haskell

--- haskell/etl/ETL.hs
+++ haskell/etl/ETL.hs
@@ -1,7 +1,8 @@
 module ETL(transform) where
 
 import Data.Char(toLower)
-import qualified Data.Map as M
+import Data.List(foldl')
+import qualified Data.Map.Strict as M
 
 type Value = Int
 type Letter = String
@@ -13,7 +14,7 @@ transform :: OldScoreSet -> NewScoreSet
 transform = M.foldrWithKey transformEach M.empty
 
 transformEach :: Value -> [Letter] -> NewScoreSet -> NewScoreSet
-transformEach score = flip (foldr newRecord)
+transformEach score = flip (foldl' newRecord)
     where
-    newRecord :: Letter -> NewScoreSet -> NewScoreSet
-    newRecord = (`M.insert` score) . map toLower
+    newRecord :: NewScoreSet -> Letter -> NewScoreSet
+    newRecord = flip ((`M.insert` score) . map toLower)
