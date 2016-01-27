8eb19f007455fe1392001cf7b076ddc03f744833
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-26 23:47:38 -0200

    anagram in lua with concat

--- lua/anagram/anagram.lua
+++ lua/anagram/anagram.lua
@@ -9,35 +9,23 @@ anagram.new = function(_, test)
 end
 
 local table_insert = table.insert
-local table_compare
-table_icompare = function(table1, table2)
-    if #table1 ~= #table2 then
-        return false
-    end
-    for k = 1, #table1 do
-        if table1[k] ~= table2[k] then
-            return false
-        end
-    end
-    return true
-end
-
-anagram.match = function(self, candidates)
-    local selecteds = {}
-    for _, candidate in ipairs(candidates) do
-        if table_icompare(anagram.sort(candidate), self.test) then
-            table_insert(selecteds, candidate)
-        end
-    end
-    return selecteds
-end
-
+local table_concat = table.concat
 anagram.sort = function(str)
     local tab = {}
     str = str:lower()
     str:gsub(".", function(char) table_insert(tab, char) end)
     table.sort(tab)
-    return tab
+    return table_concat(tab)
+end
+
+anagram.match = function(self, candidates)
+    local selecteds = {}
+    for _, candidate in ipairs(candidates) do
+        if anagram.sort(candidate) == self.test then
+            table_insert(selecteds, candidate)
+        end
+    end
+    return selecteds
 end
 
 return anagram
