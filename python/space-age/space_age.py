96c315d6ab520465d4ce99f400a5edf8f907eeb4
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-01-29 19:08:23 -0200

    refactor space_age in python
    http://docs.python-guide.org/en/latest/writing/gotchas/#late-binding-closures

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -2,26 +2,23 @@ class SpaceAge(object):
     def __init__(self, seconds):
         self.seconds = seconds
 
-    def on_earth(self):
-        return round(self.seconds / 31557600, 2)
+YEARS_SECONDS = {
+    'mercury': 31557600 * 0.2408467,
+    'venus':   31557600 * 0.6151972,
+    'earth':   31557600,
+    'mars':    31557600 * 1.8808158,
+    'jupiter': 31557600 * 11.862615,
+    'saturn':  31557600 * 29.447498,
+    'uranus':  31557600 * 84.016846,
+    'neptune': 31557600 * 164.79132,
+}
 
-    def on_mercury(self):
-        return round(self.seconds / (31557600 * 0.2408467), 2)
+for planet, year_seconds in YEARS_SECONDS.items():
+    def make_method(year_seconds=year_seconds):
+        def on_planet(self):
+            return round(self.seconds / year_seconds, 2)
+        return on_planet
 
-    def on_venus(self):
-        return round(self.seconds / (31557600 * 0.61519726), 2)
+    setattr(SpaceAge, 'on_' + planet, make_method())
 
-    def on_mars(self):
-        return round(self.seconds / (31557600 * 1.8808158), 2)
-
-    def on_jupiter(self):
-        return round(self.seconds / (31557600 * 11.862615), 2)
-
-    def on_saturn(self):
-        return round(self.seconds / (31557600 * 29.447498), 2)
-
-    def on_uranus(self):
-        return round(self.seconds / (31557600 * 84.016846), 2)
-
-    def on_neptune(self):
-        return round(self.seconds / (31557600 * 164.79132), 2)
+# http://docs.python-guide.org/en/latest/writing/gotchas/#late-binding-closures
