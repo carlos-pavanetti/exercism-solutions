fd82e15ecdfbe76ce6ccf20b3fc92f33be5c0167
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-05 23:46:45 -0200

    refactor of rna transcription in lua

--- lua/rna-transcription/rna-transcription.lua
+++ lua/rna-transcription/rna-transcription.lua
@@ -2,16 +2,15 @@ local concat, insert = table.concat, table.insert
 
 return function(dna_strand)
     local rna_nucleotides = {}
+    local rna_conversion = {
+        A = 'U',
+        T = 'A',
+        G = 'C',
+        C = 'G',
+    }
+
     for nucleotide in dna_strand:gmatch('.') do
-        if nucleotide == 'A' then
-            insert(rna_nucleotides, 'U')
-        elseif nucleotide == 'T' then
-            insert(rna_nucleotides, 'A')
-        elseif nucleotide == 'G' then
-            insert(rna_nucleotides, 'C')
-        elseif nucleotide == 'C' then
-            insert(rna_nucleotides, 'G')
-        end
+        insert(rna_nucleotides, rna_conversion[nucleotide])
     end
 
     return concat(rna_nucleotides)
