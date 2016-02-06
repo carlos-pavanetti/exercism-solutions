69a81a58c771cd37cb5d906ab217fc8253a90169
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-05 23:51:09 -0200

    refactor nucleotide count

--- lua/nucleotide-count/nucleotide-count.lua
+++ lua/nucleotide-count/nucleotide-count.lua
@@ -3,23 +3,18 @@ local dna_mt = {__index = dna}
 
 dna.new = function(_, strand)
     local self = {}
-    self.strand = strand
 
     local nucleotide_counts = { A = 0, T = 0, C = 0, G = 0 }
-
     for nucleotide in strand:gmatch('[ACGT]') do
         nucleotide_counts[nucleotide] = nucleotide_counts[nucleotide] + 1
     end
-    self.nucleotideCounts = nucleotide_counts
 
+    self.nucleotideCounts = nucleotide_counts
     return setmetatable (self, dna_mt)
 end
 
 dna.count = function(self, nucleotide)
-    if not ('ACGT'):find(nucleotide) then
-        error "Invalid Nucleotide"
-    end
-
+    assert(('ACGT'):find(nucleotide),  "Invalid Nucleotide")
     return self.nucleotideCounts[nucleotide]
 end
 
