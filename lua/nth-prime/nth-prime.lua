10c76d4223b223e69d1a19b1c4cf698faa8a4081
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-07 14:23:33 -0200

    refactor of nth prime in lua

--- lua/nth-prime/nth-prime.lua
+++ lua/nth-prime/nth-prime.lua
@@ -4,19 +4,14 @@ return function(n)
 
     local prime_list = {2, 3, 5, 7, 11, 13}
 
-    if n <= 6 then
-        return prime_list[n]
-    end
-
     local is_prime = function(candidate)
         for _, prime in ipairs(prime_list) do
             if prime^2 > candidate then
-                break
+                return true
             elseif candidate % prime == 0 then
                 return false
             end
         end
-        return true
     end
 
     local generate_prime_candidate = coroutine.wrap(function()
@@ -28,12 +23,10 @@ return function(n)
         end
     end)
 
-    local primes_remaining = n - 6
-    while primes_remaining > 0 do
+    while #prime_list < n do
         local candidate = generate_prime_candidate()
         if is_prime(candidate) then
             insert(prime_list, candidate)
-            primes_remaining = primes_remaining - 1
         end
     end
 
