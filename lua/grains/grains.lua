07d11e698324f1c3e09e87d4b5ddeda474aef6e9
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-06 22:26:43 -0200

    grains in lua

---
+++ lua/grains/grains.lua
@@ -0,0 +1,22 @@
+local M = {}
+if setfenv then
+    -- Lua 5.1
+    setfenv(1, M)
+else
+    -- Lua 5.2 or greater =D
+    _ENV = M
+end
+
+square = function(sq)
+    return 1 << (sq - 1)
+end
+
+total_until = function(sq)
+    return (1 << sq) - 1
+end
+
+total = function()
+    return total_until(64)
+end
+
+return M
