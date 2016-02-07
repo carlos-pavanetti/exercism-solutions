233c53e4e9cc287c93e41be34e3c400c2a16eedc
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-07 19:55:12 -0200

    nth prime in lua with memoization

--- lua/nth-prime/nth-prime.lua
+++ lua/nth-prime/nth-prime.lua
@@ -1,27 +1,28 @@
+local prime_list = {2, 3, 5, 7, 11, 13}
+
+local is_prime = function(candidate)
+    for _, prime in ipairs(prime_list) do
+        if prime^2 > candidate then
+            return true
+        elseif candidate % prime == 0 then
+            return false
+        end
+    end
+end
+
+local coroutine, yield = coroutine, coroutine.yield
+local generate_prime_candidate = coroutine.wrap(function()
+    local k = 3
+    while true do
+        yield(6*k-1)
+        yield(6*k+1)
+        k = k + 1
+    end
+end)
+
 local insert = table.insert
 return function(n)
     assert(n > 0, 'Should be passed a positive (>0) parameter')
-
-    local prime_list = {2, 3, 5, 7, 11, 13}
-
-    local is_prime = function(candidate)
-        for _, prime in ipairs(prime_list) do
-            if prime^2 > candidate then
-                return true
-            elseif candidate % prime == 0 then
-                return false
-            end
-        end
-    end
-
-    local generate_prime_candidate = coroutine.wrap(function()
-        local k = 3
-        while true do
-            coroutine.yield(6*k-1)
-            coroutine.yield(6*k+1)
-            k = k + 1
-        end
-    end)
 
     while #prime_list < n do
         local candidate = generate_prime_candidate()
