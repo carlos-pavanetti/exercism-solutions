03a85c5935443a1d684342cd2fdeef45fb583012
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-09 20:56:28 -0200

    bowling in lua

--- lua/bowling/bowling.lua
+++ lua/bowling/bowling.lua
@@ -1,69 +1,61 @@
-local function sum(list)
-    local acc = 0
-    for _, value in ipairs(list) do
-        acc = acc + value
-    end
-    return acc
-end
-
-local function is_strike(frame)
-    return frame and frame[1] == 10
-end
-
-local function is_spare(frame)
-    return frame and (frame[1] or 0 + frame[2] or 0) == 10
-end
-
-
 return function()
-    -- local score = 0
     local frames = {}
+    local current_frame = {}
 
-    local last_frame = {}
-    local first_roll_in_frame = true
+    local function is_afoot()
+        return #frames < 10
+    end
 
-    -- local function sum_last_frame()
-    --     return (last_frame[1] or 0) + (last_frame[2] or 0)
-    -- end
+    local function is_complete()
+        return not is_afoot()
+    end
+
+    local function is_strike()
+        return current_frame[1] == 10
+    end
+
+    local function is_spare()
+        return current_frame[1] + current_frame[2] == 10
+    end
+
+    local function is_normal_frame()
+        return not (is_spare() or is_strike())
+    end
 
     local bowling_scorer = {}
     function bowling_scorer.roll(pins)
-        if #frames >= 10 then
-            if not (is_spare(frames[#frames]) or is_strike(frames[#frames])) then
-                error ''
+        assert(is_afoot(), 'Cannot roll once more, game is already over.')
+        assert(pins >= 0 and pins <= 10,
+              'Dropped pins should be between [0-10].')
+
+        table.insert(current_frame, pins)
+        if #current_frame > 2 then
+            local frame_score = current_frame[1]
+                              + current_frame[2]
+                              + current_frame[3]
+            table.insert(frames, frame_score)
+
+            if is_strike() then
+                current_frame = { current_frame[2], current_frame[3] }
+            elseif is_spare() then
+                current_frame = { current_frame[3] }
             end
         end
-        assert(pins >=  0, 'Dropped pins should be a non-negative number')
-        assert(pins <= 10, 'Roll cannot be better than a strike')
 
-        table.insert(last_frame, pins)
-        if not first_roll_in_frame or pins == 10 then
-            table.insert(frames, last_frame)
-
-            first_roll_in_frame = true
-            last_frame = {}
-        else
-            first_roll_in_frame = false
+        if is_afoot() and #current_frame == 2 and is_normal_frame() then
+            table.insert(frames, current_frame[1] + current_frame[2])
+            current_frame = {}
         end
     end
 
     function bowling_scorer.score()
-        assert(#frames >= 10, 'Cannot retrieve score at game beginning')
-        assert(first_roll_in_frame, '')
+        assert(is_complete(), 'Cannot retrieve score, game is not yet over.')
 
-        local frame_score = {}
-        for frame_index=#frames, 1, -1 do
-            local frame = frames[frame_index]
-
-            if is_strike(frame) and frame_index < 10 then
-                frame_score[frame_index] = 10 + frame_score[frame_index + 1]
-            elseif is_spare(frame) and frame_index < 10 then
-                frame_score[frame_index] = 10 + frames[frame_index + 1][1]
-            else
-                frame_score[frame_index] = frame[1] + (frame[2] or 0)
-            end
+        local score = 0
+        for _, frame_score in ipairs(frames) do
+            score = score + frame_score
         end
-        return sum(frame_score)
+        return score
     end
 
     return bowling_scorer
