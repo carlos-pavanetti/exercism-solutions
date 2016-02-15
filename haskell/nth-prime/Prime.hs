4a1af234967a03caf1e411f9b5a024271ab57e38
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-15 18:13:27 -0200

    nth prime in haskell with 6k generator

--- haskell/nth-prime/Prime.hs
+++ haskell/nth-prime/Prime.hs
@@ -4,7 +4,8 @@ nth :: Int -> Integer
 nth = (!!) primes . pred
 
 primes :: [Integer]
-primes = 2: 3: filter isPrime [5,7..]
+primes = 2: 3: filter isPrime primeCandidates
     where
         isPrime x = and [x `mod` d /= 0 | d <- primesLessThanSquareRoot x]
         primesLessThanSquareRoot x = takeWhile (\p -> p*p <= x) primes
+        primeCandidates = 5: 7: map (+6) primeCandidates
