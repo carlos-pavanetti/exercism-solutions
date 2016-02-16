cfdab86897bd307d9637bc7bbf6f5533db0d8533
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-16 00:00:31 -0200

    extracted methods in python simple cipher

--- python/simple-cipher/cipher.py
+++ python/simple-cipher/cipher.py
@@ -1,5 +1,5 @@
-import random
-import string
+from random import choice
+from string import ascii_lowercase
 from itertools import cycle
 
 
@@ -27,30 +27,29 @@ class Caesar(object):
 
 class Cipher(object):
     def __init__(self, key=''):
-        self.key = key or ''.join(random.choice(string.ascii_lowercase)
-                                  for _ in range(100))
+        self.key = key or Cipher.generate_random_key()
 
-        if not (self.key.islower() and self.key.isalpha()):
+        if not self.is_key_valid():
             raise ValueError
 
-    def encode(self, source):
-        encoded = []
+    def encode(self, source, inverse=False):
+        result = []
+        signal_factor = inverse and -1 or 1
         iter_key = cycle(self.key)
 
         for char in source.lower():
             if not char.isalpha():
                 continue
-            key = normalize_to_number(next(iter_key))
-            encoded.append(shift(char, key))
-        return ''.join(encoded)
+            key = normalize_to_number(next(iter_key)) * signal_factor
+            result.append(shift(char, key))
+
+        return ''.join(result)
 
     def decode(self, source):
-        decoded = []
-        iter_key = cycle(self.key)
+        return self.encode(source, inverse=True)
 
-        for char in source.lower():
-            if not char.isalpha():
-                continue
-            key = normalize_to_number(next(iter_key))
-            decoded.append(shift(char, -key))
-        return ''.join(decoded)
+    def generate_random_key(size=100):
+        return ''.join(choice(ascii_lowercase) for _ in range(size))
+
+    def is_key_valid(self):
+        return self.key.isalpha() and self.key.islower()
