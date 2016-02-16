e423555490bf58f37962d99ab6c85121825a26b4
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-16 21:51:52 -0200

    rewrite of palindromes product in haskell

--- haskell/palindrome-products/Palindromes.hs
+++ haskell/palindrome-products/Palindromes.hs
@@ -1,4 +1,48 @@
-module Palindromes where
+module Palindromes (largestPalindrome, smallestPalindrome) where
+
+-- diagonalize :: [a] -> [b] -> [(a, b)]
+-- diagonalize = diagonalizeWith (\a b -> (a, b))
+
+diagonalizeWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+diagonalizeWith f xs ys = concat $ diagonals 0
+    where
+        lastDiagonal = min (length xs) (length ys)
+        diagonals k
+            | k > lastDiagonal = []
+            | otherwise =
+                [
+                    let
+                        x = (xs !! m)
+                        y = ys !! (k - m)
+                    in
+                    f x y | m <- [0.. k], k < m + lastDiagonal, m < lastDiagonal
+                ]: diagonals (k + 1)
+
+largestPalindrome :: Integral a => a -> a -> (a, [(a, a)])
+largestPalindrome lower upper = (largest, factors)
+    where
+        factors = [(x, y)
+                  | x <- range,
+                    x `isDivisorOf` largest,
+                    let y = largest `quot` x,
+                    y >= lower,
+                    y <= upper]
+        largest = head . filter isPalindrome $ diagonalizeWith (*) range range
+        range = reverse [lower.. upper]
+        a `isDivisorOf` b = b `rem` a == 0
+
+smallestPalindrome :: Integral a => a -> a -> (a, [(a, a)])
+smallestPalindrome lower upper = (largest, factors)
+    where
+        factors = [(x, y)
+                  | x <- range,
+                    x `isDivisorOf` largest,
+                    let y = largest `quot` x,
+                    y >= lower,
+                    y <= upper]
+        largest = head . filter isPalindrome $ diagonalizeWith (*) range range
+        range = [lower.. upper]
+        a `isDivisorOf` b = b `rem` a == 0
 
 isPalindrome :: Integral a => a -> Bool
 isPalindrome number = reversed number == number
@@ -10,28 +54,3 @@ reversed number = foldl (\acc x -> 10 * acc + x) 0 (digits number)
         digits n =
             let (q, r) = n `quotRem` 10 in
             r: digits q
-
-isPalindromeProduct :: Integral a => (a, a) -> Bool
-isPalindromeProduct = isPalindrome . uncurry (*)
-
-palindromes :: Integral a => a -> a -> [(a, (a, a))]
-palindromes lower upper = [set a b | a<-range, b<-range, isPalindrome (a * b)]
-    where
-        range = [lower..upper]
-        set a b = (a * b, (a, b))
-
-largestPalindrome :: Integral a => a -> a -> (a, [(a, a)])
-largestPalindrome lower upper = (largest, factors)
-    where
-        largest = maximum pairProducts
-        factors = [snd pair | pair<-palindromesSet, largest == fst pair]
-        pairProducts = map fst palindromesSet
-        palindromesSet = palindromes lower upper
-
-smallestPalindrome :: Integral a => a -> a -> (a, [(a, a)])
-smallestPalindrome lower upper = (smallest, factors)
-    where
-        smallest = minimum pairProducts
-        factors = [snd pair | pair<-palindromesSet, smallest == fst pair]
-        pairProducts = map fst palindromesSet
-        palindromesSet = palindromes lower upper
