e51213cd7200fa96cd46c31b5e5bfb88d232fbce
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-15 23:52:34 -0200

    refactor of simple cipher in python

--- python/simple-cipher/cipher.py
+++ python/simple-cipher/cipher.py
@@ -3,23 +3,35 @@ import string
 from itertools import cycle
 
 
+def normalize_to_number(char):
+    remainder = ord('a') % 26
+    return (ord(char.lower()) - remainder) % 26
+
+
+def normalize_to_char(number):
+    ord_a = ord('a')
+    return chr(number % 26 + ord_a)
+
+
+def shift(char, key):
+    return normalize_to_char(key + normalize_to_number(char))
+
+
 class Caesar(object):
     def encode(self, source):
-        return ''.join(chr((ord(char.lower()) - 19 + 3) % 26 + 97)
-                       for char in source if char.isalpha())
+        return ''.join(shift(char, 3) for char in source if char.isalpha())
 
     def decode(self, source):
-        return ''.join(chr((ord(char) - 19 - 3) % 26 + 97) for char in source)
+        return ''.join(shift(char, -3) for char in source)
 
 
 class Cipher(object):
     def __init__(self, key=''):
-        key = key or ''.join(random.choice(string.ascii_lowercase)
-                             for _ in range(100))
-        if not (key.islower() and key.isalpha()):
-            raise ValueError
+        self.key = key or ''.join(random.choice(string.ascii_lowercase)
+                                  for _ in range(100))
 
-        self.key = key
+        if not (self.key.islower() and self.key.isalpha()):
+            raise ValueError
 
     def encode(self, source):
         encoded = []
@@ -28,8 +40,8 @@ class Cipher(object):
         for char in source.lower():
             if not char.isalpha():
                 continue
-            key = ord(next(iter_key))
-            encoded.append(chr((ord(char) + key - 12) % 26 + 97))
+            key = normalize_to_number(next(iter_key))
+            encoded.append(shift(char, key))
         return ''.join(encoded)
 
     def decode(self, source):
@@ -39,6 +51,6 @@ class Cipher(object):
         for char in source.lower():
             if not char.isalpha():
                 continue
-            key = ord(next(iter_key))
-            decoded.append(chr((ord(char) - key) % 26 + 97))
+            key = normalize_to_number(next(iter_key))
+            decoded.append(shift(char, -key))
         return ''.join(decoded)
