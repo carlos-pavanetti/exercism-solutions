0d363e96e50e57d146d4d198dbb8d5987a0ea0a4
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-22 21:14:01 -0300

    refactor of translate function of pig-latin in lua

--- lua/pig-latin/pig-latin.lua
+++ lua/pig-latin/pig-latin.lua
@@ -5,11 +5,11 @@ local function process_word(word)
     local first_letter, tail = word:match('(.)(.+)')
     local two_first_letters, rest_two = word:match('(..)(.+)')
     local three_first_letters, rest_three = word:match('(...)(.+)')
-    if vowels[first_letter:lower()] or vowels[two_first_letters:lower()] then
+    if vowels[first_letter] or vowels[two_first_letters] then
         return word .. 'ay'
-    elseif three_first_letters and specials_consonants[three_first_letters:lower()] then
+    elseif specials_consonants[three_first_letters] then
         return (rest_three or '') .. three_first_letters .. 'ay'
-    elseif specials_consonants[two_first_letters:lower()] then
+    elseif specials_consonants[two_first_letters] then
         return (rest_two or '') .. two_first_letters .. 'ay'
     elseif tail:match('^qu') then
         return tail:sub(3) .. first_letter .. 'quay'
@@ -19,14 +19,5 @@ local function process_word(word)
 end
 
 return function(phrase)
-    local phrase_slices = {}
-    for word in phrase:gmatch('%S+') do
-        if word:match('%W') then
-            table.insert(phrase_slices, word)
-        else
-            table.insert(phrase_slices, process_word(word))
-        end
-    end
-
-    return table.concat(phrase_slices, ' ')
+    return phrase:gsub('%a+', process_word)
 end
