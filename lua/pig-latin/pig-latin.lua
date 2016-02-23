da3fea1699d880834d4ab84ab90e40dd219566c6
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-23 19:27:02 -0300

    refactor of pig-latin

--- lua/pig-latin/pig-latin.lua
+++ lua/pig-latin/pig-latin.lua
@@ -1,28 +1,31 @@
-function string.begin_with_any_of(word, patterns)
+function string:begin_with_any_of(patterns)
     for _, pattern in ipairs(patterns) do
-        if word:find('^' .. pattern) then
+        if self:find('^' .. pattern) then
             return true
         end
     end
 end
 
-function string.reverse_split_at(str, pos)
-    return str:sub(pos + 1) .. str:sub(1, pos)
+function string:reverse_split_at(pos)
+    return self:sub(pos + 1) .. self:sub(1, pos)
 end
 
 local vowel_patterns = { '[aeiou]', 'yt', 'xr' }
-local three_consonants_patterns = { '[^aeiou]qu', 'thr', 'sch' }
-local two_consonants_patterns = { 'qu', 'ch', 'th' }
+local consonant_patterns = {
+    { size = 3, '[^aeiou]qu', 'thr', 'sch' },
+    { size = 2, 'qu', 'ch', 'th' },
+    { size = 1, '[^aeiou]' }
+}
 
 local function process_word(word)
     if word:begin_with_any_of(vowel_patterns) then
         return word .. 'ay'
-    elseif word:begin_with_any_of(three_consonants_patterns) then
-        return word:reverse_split_at(3) .. 'ay'
-    elseif word:begin_with_any_of(two_consonants_patterns) then
-        return word:reverse_split_at(2) .. 'ay'
     else
-        return word:reverse_split_at(1) .. 'ay'
+        for _, patterns in ipairs(consonant_patterns) do
+            if word:begin_with_any_of(patterns) then
+                return word:reverse_split_at(patterns.size) .. 'ay'
+            end
+        end
     end
 end
 
