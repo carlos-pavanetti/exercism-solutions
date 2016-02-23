ac40d8dd6aa3a9fd9add6b65cf3b751802139314
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-23 19:16:35 -0300

    modified:   pig-latin.lua

--- lua/pig-latin/pig-latin.lua
+++ lua/pig-latin/pig-latin.lua
@@ -1,5 +1,3 @@
-local specials_consonants = { ch = true, qu = true, th = true, thr = true, sch = true}
-
 function string.begin_with_any_of(word, patterns)
     for _, pattern in ipairs(patterns) do
         if word:find('^' .. pattern) then
@@ -12,17 +10,19 @@ function string.reverse_split_at(str, pos)
     return str:sub(pos + 1) .. str:sub(1, pos)
 end
 
+local vowel_patterns = { '[aeiou]', 'yt', 'xr' }
+local three_consonants_patterns = { '[^aeiou]qu', 'thr', 'sch' }
+local two_consonants_patterns = { 'qu', 'ch', 'th' }
+
 local function process_word(word)
-    local first_letter, tail = word:match('(.)(.+)')
-    local two_first_letters, rest_two = word:match('(..)(.+)')
-    if word:begin_with_any_of({ '[aeiou]', 'yt', 'xr' }) then
+    if word:begin_with_any_of(vowel_patterns) then
         return word .. 'ay'
-    elseif word:begin_with_any_of({ '[^aeiou]qu', 'thr', 'sch' }) then
+    elseif word:begin_with_any_of(three_consonants_patterns) then
         return word:reverse_split_at(3) .. 'ay'
-    elseif word:begin_with_any_of({ 'qu', 'ch', 'th' }) then
+    elseif word:begin_with_any_of(two_consonants_patterns) then
         return word:reverse_split_at(2) .. 'ay'
     else
-        return tail .. first_letter .. 'ay'
+        return word:reverse_split_at(1) .. 'ay'
     end
 end
 
