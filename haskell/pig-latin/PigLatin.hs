6e13171ecf6fe61f78fe3055628a57139a36935d
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-25 14:33:53 -0300

    modified:   PigLatin.hs

--- haskell/pig-latin/PigLatin.hs
+++ haskell/pig-latin/PigLatin.hs
@@ -3,20 +3,18 @@ module PigLatin (translate) where
 import Data.Char
 import Data.List.Split
 
-processWord :: String -> String
-processWord (f:s:t:rest)
-    | f `elem` "aeiou" = f:s:t:rest ++ "ay"
-    | [f, s, t] `elem` ["thr", "sch"] = rest ++ f:s:t:"ay"
-    | [s, t] == "qu" = rest ++ f:s:t:"ay"
-    | [f, s] `elem` ["qu", "ch", "th"] = t:rest ++ f:s:"ay"
-    | otherwise = s:t:rest ++ f:"ay"
-processWord _ = undefined
-
 translate :: String -> String
 translate = concatMap processPart . splitWords
     where
         processPart part =
             if all isAlpha part
-                then processWord part
+                then reorder part ++ "ay"
                 else part
+        -- could be words, but I inspired it in the Lua pig latin problem,
+        -- where it could have non alpha chars
         splitWords = split (whenElt (not.isAlpha))
+        reorder [] = []
+        reorder ('q':'u':css) = css ++ "qu"
+        reorder (c:css)
+            | c `elem` "aeiou" = c:css
+            | otherwise = reorder (css ++ [c])
