df9795f50c47c8495256d173d725af2908a2fd77
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-02-29 18:10:12 -0300

    pythagorean triplet in haskell

--- haskell/pythagorean-triplet/Triplet.hs
+++ haskell/pythagorean-triplet/Triplet.hs
@@ -13,8 +13,9 @@ isPythagorean (Triplet a b c) = (catA*catA + catB*catB) == hypt*hypt
         catB = a + b + c - hypt - catA
 
 pythagoreanTriplets :: (Enum a, Eq a, Num a, Ord a) => a -> a -> [Triplet a]
-pythagoreanTriplets lowerBound upperBound = [Triplet ((m*m)-(n*n)) (2*m*n) ((m*m)+(n*n)) | m<-range, n<-range, inBound m n]
-    where
-        range = takeWhile (\n -> 2*n*n < upperBound) [1..]
-        inBound m n = m > n && (m*m-n*n) `elem` bound || (2*m*n) `elem` bound || (m*m+n*n) `elem` bound
-        bound = [lowerBound .. upperBound]
+pythagoreanTriplets lowerBound upperBound =
+    filter isPythagorean [Triplet a b c
+                         | a<-[lowerBound .. upperBound]
+                         , b<-[a          .. upperBound]
+                         , c<-[b          .. upperBound]
+                         ]
