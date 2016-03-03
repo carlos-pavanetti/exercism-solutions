8d849837b69701f55a0803a9d5d57f2e5c6109f3
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-03-03 00:02:27 -0300

    lua pythagoreans triplet finished

--- lua/pythagorean-triplet/pythagorean-triplet.lua
+++ lua/pythagorean-triplet/pythagorean-triplet.lua
@@ -5,8 +5,25 @@ local function is_triplet(a, b, c)
     return (hypotenuse^2) == (cathetusA^2 + cathetusB^2)
 end
 
-local function triplets_with(lower, upper)
+local function triplets_with(options)
+    local triplets = {}
 
+    local min_factor = options.min_factor or 1
+    local max_factor = options.max_factor
+    local sum        = options.sum
+    for a = min_factor, max_factor do
+        for b = a,      max_factor do
+            for c = b,  max_factor do
+                if is_triplet(a, b, c) then
+                    if not sum or a + b + c == sum then
+                        table.insert(triplets, { a, b, c })
+                    end
+                end
+            end
+        end
+    end
+
+    return triplets
 end
 
 return { is_triplet = is_triplet, triplets_with = triplets_with }
