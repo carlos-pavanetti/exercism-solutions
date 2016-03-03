cff0d036c2691d987da569c42c7c141aed8b0a24
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-03-03 01:02:57 -0300

    pythagorean triplet in lua with euclidean generator

--- lua/pythagorean-triplet/pythagorean-triplet.lua
+++ lua/pythagorean-triplet/pythagorean-triplet.lua
@@ -1,11 +1,17 @@
+local function is_table_triplet(triplet)
+    table.sort(triplet)
+    local a, b, c = table.unpack(triplet)
+    return a^2 + b^2 == c^2
+end
+
 local function is_triplet(a, b, c)
     return is_table_triplet { a, b, c }
 end
 
-local function is_table_triplet(triplet)
-    triplet:sort()
-    local a, b, c = table.unpack(triplet)
-    return a^2 + b^2 == c^2
+local function gcd(a, b)
+    if a > b then a, b = b, a end
+    if a == 0 then return b end
+    return gcd(a, b - a)
 end
 
 local function triplets_with(options)
@@ -14,16 +20,32 @@ local function triplets_with(options)
     local min_factor = options.min_factor or 3
     local max_factor = options.max_factor
 
-    for a = min_factor, max_factor do
-        for b = a,      max_factor do
-            for c = b,  max_factor do
-                if is_triplet(a, b, c) then
-                    if not options.sum or a + b + c == options.sum then
-                        table.insert(triplets, { a, b, c })
+    local m, n = 2, 1
+    local a, b, c
+
+    c = 0 -- a number to compare with max_factor
+    while c <= max_factor do
+        while n < m do
+            a = m^2 - n^2
+            b = 2 * m * n
+            c = m^2 + n^2
+            if c > max_factor then break end
+                for k = 1, max_factor/c do
+                    local abc = { k*a, k*b, k*c }
+                    if abc[1] >= min_factor and abc[2] >= min_factor then
+                    if not options.sum or options.sum == k*(a + b + c) then
+                        table.sort(abc)
+                        table.insert(triplets, abc)
                     end
                 end
             end
+            repeat
+                n = n + 2
+            until gcd(m, n) == 1 -- m & n must be coprimes
         end
+
+        m = m + 1
+        n = m % 2 + 1 -- m & n must have distinct parity
     end
 
     return triplets
