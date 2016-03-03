c298e906b74a787e86ef016f5e5c750b46dd26cc
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-03-03 00:12:52 -0300

    refactor of is_triplet

--- lua/pythagorean-triplet/pythagorean-triplet.lua
+++ lua/pythagorean-triplet/pythagorean-triplet.lua
@@ -1,21 +1,24 @@
 local function is_triplet(a, b, c)
-    local hypotenuse = math.max(a, b, c)
-    local cathetusA = math.min(a, b, c)
-    local cathetusB = a + b + c - (cathetusA + hypotenuse)
-    return (hypotenuse^2) == (cathetusA^2 + cathetusB^2)
+    return is_table_triplet { a, b, c }
+end
+
+local function is_table_triplet(triplet)
+    triplet:sort()
+    local a, b, c = table.unpack(triplet)
+    return a^2 + b^2 == c^2
 end
 
 local function triplets_with(options)
     local triplets = {}
 
-    local min_factor = options.min_factor or 1
+    local min_factor = options.min_factor or 3
     local max_factor = options.max_factor
-    local sum        = options.sum
+
     for a = min_factor, max_factor do
         for b = a,      max_factor do
             for c = b,  max_factor do
                 if is_triplet(a, b, c) then
-                    if not sum or a + b + c == sum then
+                    if not options.sum or a + b + c == options.sum then
                         table.insert(triplets, { a, b, c })
                     end
                 end
