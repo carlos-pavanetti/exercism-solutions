7bda375efa8837e26f23178929bb1cafaf721e82
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-03-05 15:00:43 -0300

    refactor of saddle points in haskell

--- haskell/saddle-points/Matrix.hs
+++ haskell/saddle-points/Matrix.hs
@@ -12,9 +12,6 @@ saddlePoints matrix = filter isSaddlePoint $ indices matrix
         isLEinColumn (i, j) =
             let elm = matrix ! (i, j)
             in and [matrix ! (x, j) >= elm | x <-iBounds]
-        jBounds =
-            let ((_, yi), (_, yn)) = bounds matrix
-            in [yi .. yn]
-        iBounds =
-            let ((xi, _), (xn, _)) = bounds matrix
-            in [xi .. xn]
+        (iBounds, jBounds) =
+            let ((xi, yi), (xn, yn)) = bounds matrix
+            in ([xi .. xn], [yi .. yn])
