e39696eddeb693f4864c8c0d13b92b50fcc53ce0
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-03-12 22:49:25 -0300

    run-length-encoding in lua

--- lua/run-length-encoding/run-length-encoding.lua
+++ lua/run-length-encoding/run-length-encoding.lua
@@ -1,9 +1,9 @@
 local function decode(channel)
     local source = {}
-    for times, char in channel:gmatch('(%d*)(%w)') do
+    for times, char in channel:gmatch('(%d*)(%D)') do
         if type(tonumber(times)) ~= 'number' then times = 1 end
 
-        for i = 1, times do
+        for _ = 1, times do
             table.insert(source, char)
         end
     end
@@ -11,7 +11,29 @@ local function decode(channel)
 end
 
 local function encode(source)
-    for char in source:gmatch('.') do
-        --
+    local channel = {}
+    local current = {}
+
+    local function push_last_char()
+        if current.count then
+            if current.count > 1 then
+                table.insert(channel, current.count)
+            end
+            table.insert(channel, current.char)
+        end
     end
+
+    for char in source:gmatch('.') do
+        if current.char == char then
+            current.count = current.count + 1
+        else
+            push_last_char()
+            current = { char = char, count = 1 }
+        end
+    end
+    push_last_char()
+
+    return table.concat(channel)
 end
+
+return { decode = decode, encode = encode }
