16fe69dfdc2969e50f3560e5b7aa2f710b152b33
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2016-03-18 12:31:40 -0300

    bob in js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -4,7 +4,7 @@ Bob.prototype.hey = function(input) {
     input = input.trim();
     if (input === '')
         return 'Fine. Be that way!';
-    if (input === input.toUpperCase())
+    if (input === input.toUpperCase() && input.search(/[a-zA-Z]/) !== -1)
         return 'Whoa, chill out!';
     if (input.slice(-1) === '?')
         return 'Sure.';
